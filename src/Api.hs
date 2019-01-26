7726bbc5b40310bd4c02bd25e578b16d65644273
kayhide <kayhide@gmail.com>
2019-01-26 17:10:53 +0900

    setup proxy server.

--- src/Api.hs
+++ src/Api.hs
@@ -2,6 +2,8 @@ module Api where
 
 import           ClassyPrelude
 
+import           Data.Aeson                (ToJSON, object, toJSON, (.=))
+import           Data.Proxy                (Proxy (..))
 import           Network.HTTP.Client       (Manager, defaultManagerSettings,
                                             newManager)
 import           Network.HTTP.ReverseProxy (ProxyDest (..),
@@ -9,16 +11,42 @@ import           Network.HTTP.ReverseProxy (ProxyDest (..),
                                             waiProxyTo)
 import           Network.Wai               (Application, Request)
 import           Network.Wai.Handler.Warp  (run)
+import           Servant                   ((:<|>) (..), (:>), Get, JSON, Proxy,
+                                            Raw, Server, serve, Tagged (..))
 
 
 forwardRequest :: Request -> IO WaiProxyResponse
 forwardRequest _ = pure $ WPRProxyDest $ ProxyDest "127.0.0.1" 5100
 
-app :: Manager -> Application
-app manager =
-  waiProxyTo forwardRequest defaultOnExc manager
-
 startApp :: IO ()
 startApp = do
   manager <- newManager defaultManagerSettings
   run 8080 $ app manager
+
+
+
+newtype Cat = Cat { cat :: String }
+  deriving Generic
+  deriving anyclass ToJSON
+
+
+type CatAPI
+  = "cat" :> Get '[JSON] Cat
+
+type API
+  = CatAPI :<|> Raw
+
+catServer :: Server CatAPI
+catServer = pure Cat { cat = "mrowl" }
+
+proxyServer :: Manager -> Application
+proxyServer = waiProxyTo forwardRequest defaultOnExc
+
+server :: Manager -> Server API
+server manager = catServer :<|> Tagged (proxyServer manager)
+
+api :: Proxy API
+api = Proxy
+
+app :: Manager -> Application
+app manager = serve api $ server manager
